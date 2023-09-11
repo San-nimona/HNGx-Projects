dfd90f78a7173d292675dab6ff60377b8d34e8a5
San-Nimona <kaybryanmc@gmail.com>
2023-09-11 15:51:19 -0500

    Stage 1

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const Current_Day_Element = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+const CurrentDate = new Date();
+const Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const CurrentDayIndex = CurrentDate.getDay();
+const CurrentdayName = Weekdays[CurrentDayIndex];
+Current_Day_Element.textContent = `${CurrentdayName}`;
+const currentTime = document.querySelector('[data-testid="currentUTCTime"]');
+currentTimeMs = new Date().getTime();
+currentTime.textContent = `${currentTimeMs}`;
